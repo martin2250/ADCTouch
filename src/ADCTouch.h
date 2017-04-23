b7b06b453c1bd8343f859cb22a90db3cbedcdd08
Facundo Daguerre <daguerref@hotmail.com>
2017-04-23 12:05:58 -0300

    Update ADCTouch.h
    stranges symbols, just re-open the file

--- src/ADCTouch.h
+++ src/ADCTouch.h
@@ -1,1 +1,20 @@
-/*  ADCTouch.h - Library for Capacittive touch sensors using only one ADC PIN  Created by martin2250, April 23, 2014.  Released into the public domain.*/#ifndef ADCT_h#define ADCT_h#include "Arduino.h"class ADCTouchClass{	public:	int read(byte ADCChannel, int samples = 100);};extern ADCTouchClass ADCTouch;#endif
+/*
+  ADCTouch.h - Library for Capacittive touch sensors using only one ADC PIN
+  Created by martin2250, April 23, 2014.
+  Released into the public domain.
+*/
+
+#ifndef ADCT_h
+#define ADCT_h
+
+#include "Arduino.h"
+
+class ADCTouchClass
+{
+	public:
+	int read(byte ADCChannel, int samples = 100);
+};
+
+extern ADCTouchClass ADCTouch;
+
+#endif
