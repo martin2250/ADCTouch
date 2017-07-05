4f8d9dba7f6c27f091451b6f3a7b768e9c9a4c03
Jiri Culik <gculik@imprimislc.com>
2017-07-05 16:23:43 -0400

    added support for ATtiny chips

--- src/ADCTouch.cpp
+++ src/ADCTouch.cpp
@@ -8,17 +8,39 @@
 
 int ADCTouchClass::read(byte ADCChannel, int samples)
 {
+#if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+	uint8_t digitalPin = analog_pin_to_digital_pin + ADCChannel;
+	if (digitalPin == NOT_A_PIN) return;
+#endif
 	long _value = 0;
 	for(int _counter = 0; _counter < samples; _counter ++)
 	{
+#if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+		// set the digital pin corresponding to the analog pin as an input pin with a pullup resistor
+		// this will start charging the capacitive element attached to that pin
+		pinMode(digitalPin, INPUT_PULLUP);
+		
+		// connect the ADC input and the internal sample and hold capacitor to ground to discharge it
+		ADMUX = (ADMUX & 0xF0) | 0b1101;
+#else
+		// set the analog pin as an input pin with a pullup resistor
+		// this will start charging the capacitive element attached to that pin
 		pinMode(ADCChannel, INPUT_PULLUP);
 		
+		// connect the ADC input and the internal sample and hold capacitor to ground to discharge it
 		ADMUX |=   0b11111;
-		ADCSRA |= (1<<ADSC); //start conversion
-		while(!(ADCSRA & (1<<ADIF))); //wait for conversion to finish
-		ADCSRA |= (1<<ADIF); //reset the flag
-		
+#endif
+		// start the conversion
+		ADCSRA |= (1 << ADSC);
+
+		// ADSC is cleared when the conversion finishes
+		while((ADCSRA & (1 << ADSC)));
+
+#if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+		pinMode(digitalPin, INPUT);
+#else
 		pinMode(ADCChannel, INPUT);
+#endif
 		_value += analogRead(ADCChannel);
 	}
 	return _value / samples;
