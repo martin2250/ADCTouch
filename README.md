308a81eed3b9505a8c7b711a522544575b82f7d0
Spence Konde <spencekonde@gmail.com>
2021-04-27 04:59:02 -0400

    Generalize to DxCore, megaTinyCore, and unreleased ATTinyCore 2.0.0

--- README.md
+++ README.md
@@ -1,26 +1,33 @@
-## ADCTouch  
+## ADCTouch
 ADCTouch is a library that allows users to create a capacitive sensor without ANY external hardware.
 
-## Purpose  
+## Purpose
 Most capacitive touch libraries require two pins and a large resistor to acquire precise readings. This library makes use of the AVRs internal wiring to get decent resolution with just a single pin.
 
-## Usage  
-`int val = ADCTouch.read(byte analogChannel, int samples = 100);`  
-* analogChannel: the pin to use, you can just use A0 - A5  
-* samples: (optional) the amount of samples to take, defaults to 100  
-...range 1 - 65535
+## Usage
+`int val = ADCTouch.read(byte analogChannel, int samples = 100);`
+* analogChannel: the pin to use, you can just use A0 - A5
+* samples: (optional) the amount of samples to take, defaults to 64.
+...range 1 - 32767, powers of two save around 50 bytes of flash and run faster.
 ...(value does not change with different amounts of samples)
 
-## How it works    
+## How it works
 To Acquire a reading, the library does the following:
 
 * charge the test pin to 5V via pullup resistor (not directly to prevent short circuits)
 * discharge the internal ~14pF capacitor
 * set the pin to tristate
 * connect the 14pF capacitor with the pin so that charge distributes evenly
-* measure the voltage of the internal cap with analogRead()  
-...if the pin has a low capacitance, the stored charge will be small as will the resulting voltage, if the external capacitance is equal to 14pF, the volatage should be ~2.5V. Even higher capacitances will result in volatges > 2.5V. The chip and arduino board already have stray capacitances that will produce an output of ~390 and just a single external wire can boost that up to 500, so you really need offset compensation.
-
+* measure the voltage of the internal cap with analogRead()
+.if the pin has a low capacitance, the stored charge will be small as will the resulting voltage, if the external capacitance is equal to 14pF, the volatage should be around 2.5V. Even higher capacitances will result in volatges > 2.5V. The chip and arduino board already have stray capacitances that will produce an output of ~390 and just a single external wire can boost that up to 500, so you really need offset compensation.
 The accuracy is really good, most often even the LSB/smalles digit can still yield usable data and just vary by a single unit between readings (at only 100 samples, more will mean even less variation). The sensitivity is phenomenal, with a large enough surface, it can sense a person in more than 2 feet distance.
 
 A more accurate explanation can be found [here](http://tuomasnylund.fi/drupal6/content/capacitive-touch-sensing-avr-and-single-adc-pin)
+
+### Note
+Depending on the geometry of the sensing element and ambient electronic noise, it is likely that the touching individual acting as a larger antenna to pick up more noise from the room is also a significant factor. One piece of evidence in support of this is the observation that with no change in whether it is being touched - or even anyone being nearby, the value returned oscillates. The swings are much wider when it is being touched, and this can still be used as method to do touch sensing, however, rather than simply returnign the average reading, it would likely be more effective if it were to return a more processed value.
+
+## Compatibility
+It is expected to work with anything based on an ATmega328p or other part which has ADMUX = 0b11111 read the internal ground reference, as well as the ATtiny85/45/25/13 on any core.
+
+It has been generalized to work with tinyAVR 0/1/2-series (megaTinyCore) and Dx-series parts (DxCore), and any other AVR devices where the core provides an ADC_GROUND channel that works with analogRead() - that allows all direct register manipulation code to be removed entirely. The stock Arduino cores do not support this.
--- src/ADCTouch.cpp
+++ src/ADCTouch.cpp
@@ -6,45 +6,91 @@
 #include "Arduino.h"
 #include "ADCTouch.h"
 
-int ADCTouchClass::read(byte ADCChannel, int samples)
+int ADCTouchClass::read(byte analogPin, int samples)
 {
-#if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__ATtiny13__)
-	uint8_t digitalPin = pgm_read_byte(analog_pin_to_digital_pin + ADCChannel);
-	if (digitalPin == NOT_A_PIN) return -1;
-#endif
-	long _value = 0;
-	for(int _counter = 0; _counter < samples; _counter ++)
-	{
-#if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__ATtiny13__)
-		// set the digital pin corresponding to the analog pin as an input pin with a pullup resistor
-		// this will start charging the capacitive element attached to that pin
-		pinMode(digitalPin, INPUT_PULLUP);
-		
-		// connect the ADC input and the internal sample and hold capacitor to ground to discharge it
-		ADMUX = (ADMUX & 0xF0) | 0b1101;
-#else
-		// set the analog pin as an input pin with a pullup resistor
-		// this will start charging the capacitive element attached to that pin
-		pinMode(ADCChannel, INPUT_PULLUP);
-		
-		// connect the ADC input and the internal sample and hold capacitor to ground to discharge it
-		ADMUX |=   0b11111;
-#endif
-		// start the conversion
-		ADCSRA |= (1 << ADSC);
+  #ifndef ADC_GROUND
+  /* this is the old error checking for pre 2.0.0 versions of ATTinyCore and other cores on the where you can't use analog and digital pin numbers interchangibly.
+   * This dreadful behavior is finally fixed in ATTinyCore 2.0.0 - and now will work for ALL parts. Checking for ADC_GROUND is a good quick test, since it's defined
+   * there, as well as megaTinyCore and DxCore - but nowhere else */
+    #if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__ATtiny13__)
+      uint8_t digitalPin = pgm_read_byte(analog_pin_to_digital_pin + analogPin);
+      if (digitalPin == NOT_A_PIN) return -1;
+    #endif
+  #endif // End t85/t13-specific check.
 
-		// ADSC is cleared when the conversion finishes
-		while((ADCSRA & (1 << ADSC)));
+  long _value = 0;
+  for(int _counter = 0; _counter < samples; _counter++) {
+    #if defined(ADC_GROUND)
+      /* Same implementation ought to work anywhere that we have the ADC_GROUND define, because that
+       * implies that there's no need to use direct register manipulation to set this up - we can
+       * use analogRead to read the ground channel, and be done with it. We don't need to do any '
+       * direct register manipulation. (though it c)
+      pinMode(analogPin,INPUT_PULLUP);
+      // connect the ADC input and the internal sample and hold capacitor to ground to discharge it
+      // We do this by reading the internal ground channel which the core exposes as ADC_GROUND.*/
+      uint8_t oldSREG=SREG;                 // this part is timing-critical, so we will want to
+      cli();                                // turn off interrupts briefly. savea SREG (making this
+                                            // safe to call when interrupts already disabled. )
+      analogRead(ADC_GROUND);               // Now turn interrupts off...
+      pinMode(analogPin,INPUT);             // turn off pullup too...
+      int16_t val = analogRead(analogPin);  // connect the discharged S&H cap to the pim and read
+      SREG=oldSREG;                         // Restore SREG (setting interrupts to what they were).
+      _value += val;                        // Add to running total of readings
+                                            // There, that was easy wasn't it?
+      /* The source of my concern around interrupts is that we would really prefer that the time
+       * between discharging the S&H cap, after which charge will begin leaking back into it
+       * (other end is connected to Vdd/2) and turn off the pullup, after which the pin and
+       * sensing element will start picking up noise and when we start the second conversion
+       * to be as close to 0 as possible. A slow interrupt would generate a bad reading, and 2 ADC
+       * readings, while on a classic AVR, that is slow by microprocessor timescales, are still
+       * well within typical safe lengths of time to keep interrupts off for. */
+    #else
+      #if (defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__ATtiny13__))
+        // set the digital pin corresponding to the analog pin as an input pin with a pullup resistor
+        // this will start charging the capacitive element attached to that pin
+        pinMode(digitalPin, INPUT_PULLUP);
 
-#if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__ATtiny13__)
-		pinMode(digitalPin, INPUT);
-		digitalWrite(digitalPin,LOW); // required due to a bug in the Digispark Arduino core, see GitHub Issue #6
-#else
-		pinMode(ADCChannel, INPUT);
-#endif
-		_value += analogRead(ADCChannel);
-	}
-	return _value / samples;
+        // connect the ADC input and the internal sample and hold capacitor to ground to discharge it
+        ADMUX = (ADMUX & 0xF0) | 0b1101;
+      #else
+        // set the analog pin as an input pin with a pullup resistor
+        // this will start charging the capacitive element attached to that pin
+        pinMode(analogPin, INPUT_PULLUP);
+
+        // connect the ADC input and the internal sample and hold capacitor to ground to discharge it
+        ADMUX |=   0b11111;
+      #endif
+      // In order for it to actually be connected, we have to take a dummy reading of ground.
+      // start the conversion (just to connect S&H cap to ground).
+      ADCSRA |= (1 << ADSC);
+      // ADSC is cleared when the conversion finishes
+      while((ADCSRA & (1 << ADSC)));
+
+      #if (defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__ATtiny13__))
+        pinMode(digitalPin, INPUT);
+      #else
+        pinMode(analogPin, INPUT);
+      #endif
+        _value += analogRead(analogPin);
+    #endif /* end non ADC_GROUND defined case */
+    }
+  if ((samples & (samples - 1)) != 0) {
+    /* oof, gotta use a software implementation of division on a 32-bit number by a 16-bit number on an 8-bit processor!
+     * note that if LTO is enabled (it probably is), and samples is a compile-time known constant power of 2, this
+     * branch will be optimized away entirely, which is what we want, because division operators are fairly large...*/
+    return _value / samples;
+  } else {
+    /* Great, samples is a power of 2 - we can do this the fast, easy way:
+     * Rightshift samples 1 bit at a time along with _value until samples = 1
+     * at which point _value(current) = _value(original)/samples(original)
+     * Probably around a dozen instrunction words or less (4 + 2 plus rjmp, compare, and conditional branch, plus maybe
+     * a few movw's to set up the return) and worst case runtime of under 200 clocks. */
+    while (samples > 1) {
+      _value >>= 1;
+      samples >>= 1;
+    }
+    return _value;
+  }
 }
 
 ADCTouchClass ADCTouch;
